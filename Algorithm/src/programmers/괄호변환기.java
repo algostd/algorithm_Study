48dcf6b3c42d8f5475f47ca5fb06c77368c30bd5
SukWooJung <ggp03016@naver.com>
2021-07-01 10:20:04 +0900

    H_Index.java

--- Algorithm/src/programmers/괄호변환기.java
+++ Algorithm/src/programmers/괄호변환기.java
@@ -2,7 +2,7 @@ package programmers;
 
 public class °ýÈ£º¯È¯±â {
 	public static void main(String[] args) {
-		String str = "[[\"100\",\"ryan\",\"music\",\"2\"],[\"200\",\"apeach\",\"math\",\"2\"],[\"300\",\"tube\",\"computer\",\"3\"],[\"400\",\"con\",\"computer\",\"4\"],[\"500\",\"muzi\",\"music\",\"3\"],[\"600\",\"apeach\",\"music\",\"2\"]]";
+		String str = "[[4, 3], [4, 2], [3, 2], [1, 2], [2, 5]]";
 		String newStr = "";
 		for (int i = 0; i < str.length(); i++) {
 			if (str.charAt(i) == '[') {
