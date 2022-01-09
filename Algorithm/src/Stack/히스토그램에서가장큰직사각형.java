726d19125dd94c956aca65559b41b7ef0cf7e68b
정석우 <66424129+SukWooJung@users.noreply.github.com>
2022-01-09 21:07:21 +0900

    [Java] BaekJoon 6549. 히스토그램에서가장큰직사각형(모노톤스택)

--- Algorithm/src/Stack/히스토그램에서가장큰직사각형.java
+++ Algorithm/src/Stack/히스토그램에서가장큰직사각형.java
@@ -4,62 +4,81 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class 히스토그램에서가장큰직사각형 {
+    private static StringBuilder sb = new StringBuilder();
+    private static int N;
+    private static String[] split;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        while (true) {
+            split = br.readLine().split(" ");
+            if (split[0].equals("0")) {
+                break;
+            }
+            N = Integer.parseInt(split[0]);
+            getAnswer();
+        }
+        System.out.println(sb);
+    }
+
+    private static void getAnswer() {
         int[] left = new int[N + 2];
         int[] right = new int[N + 2];
-        int[] h = new int[N + 2];
+        // 맨 끝값 초기화
         left[N + 1] = N + 1;
         right[N + 1] = N + 1;
 
+        // height 초기화
+        int[] height = new int[N + 2];
+        for (int i = 1; i <= N; i++) {
+            height[i] = Integer.parseInt(split[i]);
+        }
+
         // 입력과 left monotoneStack
         Stack<Integer> monotoneStack = new Stack<>();
-        for (int i = 1; i <= N; i++) {
-            h[i] = Integer.parseInt(br.readLine());
-            if (!monotoneStack.isEmpty() && h[monotoneStack.peek()] < h[i]) {
-                monotoneStack.push(i);
-                left[i] = i - 1;
+        for (int now = 0; now <= N; now++) {
+            if (height[now] == 0) {
+                monotoneStack.clear();
+                monotoneStack.push(now);
                 continue;
             }
-            while (!monotoneStack.isEmpty() && h[monotoneStack.peek()] > h[i]) { // 모노톤 스택 유지
+            if (height[monotoneStack.peek()] < height[now]) { // 현재값이 이전 값보다 크면
+                monotoneStack.push(now);
+                left[now] = now - 1;
+                continue;
+            }
+            while (height[monotoneStack.peek()] >= height[now]) { // 모노톤 스택 유지
                 monotoneStack.pop();
             }
-            if (monotoneStack.isEmpty()) {
-                left[i] = 0;
-            } else if (h[i] == h[monotoneStack.peek()]){
-                left[i] = left[monotoneStack.peek()];
-            } else {
-                left[i] = monotoneStack.peek();
-            }
-            monotoneStack.push(i);
+            left[now] = monotoneStack.peek();
+            monotoneStack.push(now);
         }
 
-        // max로 answer 구하기 && right monotoneStack
+        // right monotoneStack 과 answer 구하기
         monotoneStack.clear();
-        int answer = -1;
-        for (int i = N; i >= 0; i--) {
-            if (!monotoneStack.isEmpty() && h[monotoneStack.peek()] < h[i]) {
-                monotoneStack.push(i);
-                right[i] = i + 1;
-                answer = Math.max(answer, (right[i] - left[i] - 1) * h[i]);
+        long answer = 0;
+        for (int now = N + 1; now >= 0; now--) {
+            if (height[now] == 0) { // 0일때 스택 초기화 및 해당 인덱스 push
+                monotoneStack.clear();
+                monotoneStack.push(now);
                 continue;
             }
-            while (!monotoneStack.isEmpty() && h[monotoneStack.peek()] > h[i]) { // 모노톤 스택 유지
+            if (height[monotoneStack.peek()] < height[now]) { // 현재값이 이전 값보다 크면
+                monotoneStack.push(now);
+                right[now] = now + 1;
+                answer = Math.max(answer, (long) (right[now] - left[now] - 1) * height[now]);
+                continue;
+            }
+            while (height[monotoneStack.peek()] >= height[now]) { // 모노톤 스택 유지
                 monotoneStack.pop();
             }
-            if (monotoneStack.isEmpty()) { // 스택에 없으면 값이 맨끝값으로
-                right[i] = N + 1;
-            } else if(h[i] == h[monotoneStack.peek()]){ // 값이 있는데 같으면 그 값은 오른쪽 제일 낮은곳으로
-                right[i] = right[monotoneStack.peek()];
-            } else { // 값이 있는데 나보다 낮으면 해당값이 right가 된다.
-                right[i] = monotoneStack.peek();
-            }
-            monotoneStack.push(i);
-            answer = Math.max(answer, (right[i] - left[i] - 1) * h[i]);
+            right[now] = monotoneStack.peek();
+            monotoneStack.push(now);
+
+            // 정답 구하기
+            answer = Math.max(answer, (long) (right[now] - left[now] - 1) * height[now]);
         }
 
-        System.out.println(answer);
+        sb.append(answer).append("\n");
     }
 }
