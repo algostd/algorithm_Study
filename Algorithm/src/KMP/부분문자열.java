eb2b0ac8a0c1dceb98dfa0ff5ff52173e328125a
정석우 <ggp03016@naver.com>
2022-01-12 00:35:52 +0900

    [Java] BaekJoon 16916. 부분문자열 주석추가

--- Algorithm/src/KMP/부분문자열.java
+++ Algorithm/src/KMP/부분문자열.java
@@ -19,31 +19,37 @@ public class 부분문자열 {
         System.out.println(result);
     }
 
+    // Pattern 문자열 기준, 0부터 해당 인덱스(i)까지 prefix 와 suffix 가 최대 몇 개 일치하는지
     private static void getLongestPreEqualSuf() {
-        int equalCount = 0;
-        for (int i = 1; i < pattern.length(); i++) {
-            while (equalCount > 0 && pattern.charAt(i) != pattern.charAt(equalCount)) {
-                equalCount = longestPreEqualSuf[equalCount - 1];
+        int equalLength = 0;
+        for (int i = 0; i < pattern.length(); i++) {
+            // prefix 와 suffix 가 어긋나는 경우, 일치했던 곳들을 확인학 roll back
+            // 어디까지 ? => 어긋난 문자와 일치하는 곳까지 or 0까지
+            while (equalLength > 0 && pattern.charAt(equalLength) != pattern.charAt(i)) {
+                equalLength = longestPreEqualSuf[equalLength - 1];
             }
-            if (pattern.charAt(i) == pattern.charAt(equalCount)) {
-                equalCount++;
-                longestPreEqualSuf[i] = equalCount;
+
+            // 인덱스의 문자와 equalLength 부분의 문자가 같으면
+            if (pattern.charAt(i) == pattern.charAt(equalLength)) {
+                equalLength++;
+                longestPreEqualSuf[i] = equalLength;
             }
         }
     }
 
     private static int KMP() {
-        int equalCount = 0;
+        int equalLength = 0;
         for (int i = 0; i < str.length(); i++) {
-            while (equalCount > 0 && str.charAt(i) != pattern.charAt(equalCount)) {
-                equalCount = longestPreEqualSuf[equalCount - 1];
+            // 찾는 문자가 다르면 roll back
+            while (equalLength > 0 && str.charAt(i) != pattern.charAt(equalLength)) {
+                equalLength = longestPreEqualSuf[equalLength - 1];
             }
-
-            if (str.charAt(i) == pattern.charAt(equalCount)) {
-                if (equalCount == pattern.length() - 1) {
+            // 찾는 문자가 같으면
+            if (str.charAt(i) == pattern.charAt(equalLength)) {
+                if (equalLength == pattern.length() - 1) {
                     return 1;
                 }
-                equalCount++;
+                equalLength++;
             }
         }
         return 0;
